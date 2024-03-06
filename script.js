893776ec710a33cc8fedc06f7e8e41b75dc221d3
Luiz Madrid <luizmadrid.dev@gmail.com>
2024-03-06 12:26:12 -0300

    feat: Adicionado projetos, e feita mudanças no layout.

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
-/* Sessão do scroll */
-
 window.addEventListener("scroll", function () {
-  /* Sticky navbar */
   let menu = document.querySelector(".menu");
   menu.classList.toggle("sticky", window.scrollY > 50);
 });
 
-/* Sessão de copiar ao clicar */
+document.getElementById("email").addEventListener("click", () => {
+  let emailContent = document.getElementById("email").textContent;
+  navigator.clipboard.writeText(emailContent);
 
-document.addEventListener("click", function (event) {
-  if (event.target.tagName === "SPAN") {
-    let copyText = event.target.textContent;
-    navigator.clipboard.writeText(copyText);
+  document.getElementById("email").style.display = "none";
+
+  if (document.getElementById("email").style.display === "none") {
+    document.getElementById("email").insertAdjacentHTML("afterend", "<p class='conteudo-contato'>Copiado!</p>");
   }
+
+  setTimeout(() => {
+    document.getElementById("email").style.display = "flex";
+    document.getElementById("email").nextElementSibling.remove();
+  }, 2000);
 });
+
+document.getElementById("whatsapp").addEventListener("click", () => {
+  let whatsappContent = document.getElementById("whatsapp").textContent;
+  navigator.clipboard.writeText(whatsappContent);
+
+  document.getElementById("whatsapp").style.display = "none";
+
+  if (document.getElementById("whatsapp").style.display === "none") {
+    document.getElementById("whatsapp").insertAdjacentHTML("afterend", "<p class='conteudo-contato'>Copiado!</p>");
+  }
+
+  setTimeout(() => {
+    document.getElementById("whatsapp").style.display = "flex";
+    document.getElementById("whatsapp").nextElementSibling.remove();
+  }, 2000);
+});
+
+const birthDate = new Date(2000, 9, 12);
+const today = new Date();
+const ageInMilliseconds = today - birthDate;
+const ageInYears = Math.floor(ageInMilliseconds / (1000 * 60 * 60 * 24 * 365.25));
+document.getElementById("age").textContent = `Tenho ${ageInYears} anos, sou natural de Araçatuba/SP.`;
